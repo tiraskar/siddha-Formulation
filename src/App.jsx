0ba8ed13c2a974a4f94ae252610f4ca32de10ad2
Bishwash28 <rijalbishwash199@gmail.com>
2025-12-11 21:58:05 +0545

    delete commented productdetailanddata

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { Router } from "react-router-dom";
 import "./App.css";
-import ViewProduct from "./pages/productdetail";
-import ProductList from "./pages/productlist";
-import ProductsDtl from "./pages/productsde";
+
 import RouterContainer from "./pages/route";
 
 function App() {
   return (
     <>
-      {/* <ProductDetail /> */}
-      {/* <ProductPage /> */}
-      {/* <ProductList /> */}
-      {/* <ProductsDtl /> */}
       <RouterContainer />
     </>
   );
